12d3f2e468ad2678d90fc92c11f3a5d79a0ac017
marcela rocha <marcelarochamartins@gmail.com>
2023-11-14 14:58:43 +0100

    post component styles

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -18,21 +18,34 @@ export function Post(props) {
           Publicado há 1h
         </time>
       </header>
+
       <div className={styles.content}>
         <p>Fala galeraa 👋 </p>
         <p>
           Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
-          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare  🚀
+          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
         </p>
         <p>
-          👉 {' '}{'     '}<a href="">jane.design/doctorcare</a>{' '}
+          👉 {"     "}
+          <a href="">jane.design/doctorcare</a>{" "}
         </p>
         <p>
-          <a href="">#novoprojeto</a>{' '}
-          <a href="">#nlw</a>{' '}
+          <a href="">#novoprojeto</a> <a href="">#nlw</a>{" "}
           <a href="">#rocketseat</a>
         </p>
       </div>
+
+      <form className={styles.commentForm}>
+
+        <strong>Give your feedback</strong>
+
+        <textarea placeholder="Leave a comment here"></textarea>
+        
+        <footer>
+        <button type="submit">Comment</button>
+        </footer>
+
+      </form>
     </article>
   );
 }
