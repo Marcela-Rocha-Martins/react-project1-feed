f1c98e9ec11a48610d8939ba267ed5715145e1d5
marcela rocha <marcelarochamartins@gmail.com>
2023-11-15 15:31:29 +0100

    useState

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,47 @@ import "./global.css";
 
 import styles from "./App.module.css";
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl:
+        "https://media.licdn.com/dms/image/C4E03AQE2AmnSI0hM0Q/profile-displayphoto-shrink_400_400/0/1632096337874?e=1705536000&v=beta&t=l7AixMgnXItY48lN17Z54yhINjRT3H_9A3ZONYtJ5oA",
+      name: "Marcela Rocha",
+      role: "Web Developer",
+    },
+    content: [
+      { type: "paragraph", content: "Hello everyone! 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Just uploaded another project to my portfolio. It's a project I worked on during the NLW Return, a Rocketseat event. The project is called DoctorCare 🚀",
+      },
+      { type: "link", content: "jane.design/doctorcare" },
+    ],
+    publishedAt: new Date("2023-11-10 20:00:00"),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl:
+        "https://images.unsplash.com/photo-1508214751196-bcfd4ca60f91?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      name: "Mary Abbot",
+      role: "UX designer",
+    },
+    content: [
+      { type: "paragraph", content: "Hey folks!" },
+      {
+        type: "paragraph",
+        content:
+          "I finally completed my new website/portfolio. It was a huge challenge to create the entire design and code it from scratch, but I made it 💪🏻",
+      },
+      { type: "link", content: "devonlane.design" },
+    ],
+    publishedAt: new Date("2023-11-10 14:00:00"),
+  },
+];
+
 function App() {
   return (
     <div className="App">
@@ -14,8 +55,15 @@ function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post />
-          <Post />
+          {posts.map((post) => {
+            return (
+              <Post
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            );
+          })}
         </main>
       </div>
     </div>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,42 +1,56 @@
+import { useState } from "react";
 import { Avatar } from "./Avatar";
 import { Comment } from "./Comment";
 import styles from "./Post.module.css";
+import { format, formatDistanceToNow } from "date-fns";
 
-export function Post(props) {
+
+export function Post({ author, publishedAt, content }) {
+  const publishedDateFormatted = format(publishedAt, "d LLLL 'at' HH:mm' h'");
+  const publishedDateRelative = formatDistanceToNow(publishedAt, {
+    addSuffix: true,
+  });
+  const [comments, setComments] = useState([1, 2, 3]);
+
+
+  function handleCreateNewComment() {
+    event.preventDefault();
+    setComments([...comments, comments.length + 1]);
+  }
+
   return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <Avatar
-            src="https://media.licdn.com/dms/image/C4E03AQE2AmnSI0hM0Q/profile-displayphoto-shrink_800_800/0/1632096337874?e=1705536000&v=beta&t=59WfAAJyMzunXUg994zXDNoqd3CtWA7Zq6lqL059KQY"
-          />
+          <Avatar src={author.avatarUrl} />
           <div className={styles.authorInfo}>
-            <strong>Marcela Rocha</strong>
-            <span>Web Developer</span>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
           </div>
         </div>
-        <time title="11 de maio às 08:13h" dateTime="2022-05-11 08:13:00">
-          Publicado há 1h
+        <time
+          title={publishedDateFormatted}
+          dateTime={publishedAt.toISOString()}
+        >
+          {publishedDateRelative}
         </time>
       </header>
 
       <div className={styles.content}>
-        <p>Fala galeraa 👋 </p>
-        <p>
-          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
-          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
-        </p>
-        <p>
-          👉 {"     "}
-          <a href="">jane.design/doctorcare</a>{" "}
-        </p>
-        <p>
-          <a href="">#novoprojeto</a> <a href="">#nlw</a>{" "}
-          <a href="">#rocketseat</a>
-        </p>
+        {content.map((aContent) => {
+          if (aContent.type === "paragraph") {
+            return <p>{aContent.content}</p>;
+          } else if (aContent.type === "link") {
+            return (
+              <p>
+                <a href="">{aContent.content}</a>
+              </p>
+            );
+          }
+        })}
       </div>
 
-      <form className={styles.commentForm}>
+      <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
         <strong>Give your feedback</strong>
 
         <textarea placeholder="Leave a comment here"></textarea>
@@ -47,9 +61,9 @@ export function Post(props) {
       </form>
 
       <div ClassName={styles.commentList}>
-        <Comment />
-        <Comment />
-        <Comment />
+        {comments.map((comment) => {
+          return <Comment />;
+        })}
       </div>
     </article>
   );
